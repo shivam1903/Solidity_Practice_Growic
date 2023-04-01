b0dc1faf07a714cefbb1d00bacdfd0372c56e7b6
shivam1903 <shivam234agarwal@gmail.com>
2023-04-02 02:10:40 +0530

    Adding first ERC20 token

---
+++ hardhat_erc20/contracts/ShivamToken.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract ShivamToken is ERC20{
+    constructor() ERC20("ShivamToken", "ST"){
+
+    }
+}
